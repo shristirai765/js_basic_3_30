2aaa62e704e05c25bba39079e418fc7e379cfabc
Sagar Bhandari <rsaagar7200@gmail.com>
2026-05-28 17:07:24 +0545

    add: control statements

--- 3.operators.js
+++ 3.operators.js
@@ -1,98 +1,136 @@
 //! operators
 
-//* arithmetic operators
-//? + , - , * , / , % , **
-console.log(2 + 12);
+// //* arithmetic operators
+// //? + , - , * , / , % , **
+// console.log(2 + 12);
 
-let sum = 12 + 2;
+// let sum = 12 + 2;
 
-let a = 34;
-let b = 23;
+// let a = 34;
+// let b = 23;
 
-console.log(a + b); //
+// console.log(a + b); //
 
-console.log(11 / 2);
-console.log(11 % 2);
-console.log(2 ** 4);
+// console.log(11 / 2);
+// console.log(11 % 2);
+// console.log(2 ** 4);
 
-console.log("hello" + " " + "world");
+// console.log("hello" + " " + "world");
 
-//* assignment
-//? = , += , -=
+// //* assignment
+// //? = , += , -=
 
-let c = 10;
-let d = 10;
+// let c = 10;
+// let d = 10;
 
-c += 5; //c = c + 5;
+// c += 5; //c = c + 5;
 
-// c = c + d;
-c -= d; // 5
+// // c = c + d;
+// c -= d; // 5
 
-console.log(c); //
+// console.log(c); //
 
-//* comparision
-//? == , === , < , > , <= >= ,  != , !==
-//? output -> boolean
+// //* comparision
+// //? == , === , < , > , <= >= ,  != , !==
+// //? output -> boolean
 
-// console.log(10 < 10); // false
-// console.log(10 <= 10); // true
+// // console.log(10 < 10); // false
+// // console.log(10 <= 10); // true
 
-let x = 10;
-let y = "10";
+// let x = 10;
+// let y = "10";
 
-console.log(x == y);
-console.log(x === y);
+// console.log(x == y);
+// console.log(x === y);
 
-//* logical
-//? and && , or || , not !
+// //* logical
+// //? and && , or || , not !
 
-console.log(true && true); //
-console.log(true && false); //
-console.log(true || true); //
-console.log(true || false); //
-console.log(false || false); //
-console.log(!true); //
-//
+// console.log(true && true); //
+// console.log(true && false); //
+// console.log(true || true); //
+// console.log(true || false); //
+// console.log(false || false); //
+// console.log(!true); //
+// //
 
-console.log(false && "hello");
+// console.log(false && "hello");
 
-let isAdmin = false;
-let res = isAdmin && "Admin";
-// console.log(res);
+// let isAdmin = false;
+// let res = isAdmin && "Admin";
+// // console.log(res);
 
-x = "abc";
-//* typeof
-console.log("typeof");
-console.log(typeof x === "number");
-console.log(typeof x);
+// x = "abc";
+// //* typeof
+// console.log("typeof");
+// console.log(typeof x === "number");
+// console.log(typeof x);
 
-// object = 1
-// number = 2
-// null = 1
+// // object = 1
+// // number = 2
+// // null = 1
 
-//* bitwise
-//  & , | ~
-console.log(2 & 3);
-// 010 & 011
+// //* bitwise
+// //  & , | ~
+// console.log(2 & 3);
+// // 010 & 011
 
-//! unary
-// increment  ++
-let z = 10;
-// post
-console.log(z++); // 10
-//  pre
-console.log(++z); // 12
+// //! unary
+// // increment  ++
+// let z = 10;
+// // post
+// console.log(z++); // 10
+// //  pre
+// console.log(++z); // 12
 
-// decrement  --
+// // decrement  --
 
-//! ternary
-//? conditioin ? exp_1 : exp_2
-let age = 17;
+// //! ternary
+// //? conditioin ? exp_1 : exp_2
+// let age = 17;
 
-let result = age >= 18 ? "can vote" : "can not vote";
+// let result = age >= 18 ? "can vote" : "can not vote";
 
-console.log(result);
+// console.log(result);
+
+//! type conversion
 
 //todo: type conversion
-//todo: type coearcing
-//todo: truthy & falsy values
+//? explicit
+console.log(Boolean(0)); //
+console.log(Number("12rs"));
+console.log(Number("abc"));
+console.log(String(true));
+//* parseInt
+console.log(parseInt("123.23"));
+console.log(parseInt("123rs"));
+
+//* parseFloat
+console.log(parseFloat("123.23"));
+
+//? implicit
+
+//todo: type coaercing
+console.log(10 + "12"); //
+console.log("10" - 4);
+console.log("qbd" - 4);
+
+//! truthy & falsy values
+//? false , 0 ,-0 ,null , undefined , NaN , ''
+
+console.log(Boolean(-0));
+
+// if (10 < 12) {
+//   console.log("if");
+// }
+
+// let user = findByid(id);
+let user = null;
+
+// if (user === null || user === undefined) {
+//   throw new Error("user not found");
+// }
+
+if (!user) {
+  throw new Error("user not found");
+}
---
+++ 4.control-statements.js
@@ -0,0 +1,113 @@
+//! control flow
+
+//! control statements
+
+//* if
+let age = 18;
+if (age) {
+  console.log("age is ", 17);
+}
+
+//* if else
+if (age >= 18) {
+  console.log("can vote");
+} else {
+  console.log("can not vote");
+}
+
+//* if-else  ladder / else if
+if (age >= 60) {
+  console.log("Major");
+} else if (age >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Minor");
+}
+
+//* switch-case
+// let day = 9;
+
+// switch (day) {
+//   case 1: {
+//     console.log("Sunday");
+//     break;
+//   }
+//   case 2: {
+//     console.log("Monday");
+//     break;
+//   }
+//   case 3: {
+//     console.log("Tuesday");
+//     break;
+//   }
+//   case 4: {
+//     console.log("Wednesday");
+//     break;
+//   }
+//   case 5: {
+//     console.log("Thursday");
+//     break;
+//   }
+//   case 6: {
+//     console.log("Friday");
+//     break;
+//   }
+//   case 7: {
+//     console.log("Saturday");
+//     break;
+//   }
+//   default: {
+//     console.log("Enter day between 1-7");
+//   }
+// }
+
+let day = 6;
+
+switch (day) {
+  case 1:
+  case 7: {
+    console.log("Weekend");
+    break;
+  }
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6: {
+    console.log("working day");
+    break;
+  }
+
+  default: {
+    console.log("Enter day between 1-7");
+  }
+}
+
+// 1 & 7 weekend ,, working day
+
+//? loops
+//* while loop
+let i = 11;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+//* do-while loop
+let j = 11;
+console.log("do-while");
+do {
+  console.log(j);
+  j++;
+} while (j <= 10);
+
+//* for loop
+console.log("for loop");
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+//* for-of
+
+//* for-in
